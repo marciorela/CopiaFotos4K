02f90983fa508f4e158ea7ef242b7c04ea3d0443
Rela <rela@home.local>
2020-01-15 14:09:32 -0300

    Versão inicial

---
+++ CopiaFotos4K/frmPrincipal.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace CopiaFotos4K
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFolder.Location = new System.Drawing.Point(15, 25);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(530, 20);
+            this.txtFolder.TabIndex = 0;
+            this.txtFolder.Text = "\\\\vserver\\temp\\fotos";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pasta";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(551, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 51);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(611, 342);
+            this.listBox1.TabIndex = 3;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 403);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFolder);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Copia Fotos 4K";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ CopiaFotos4K/frmPrincipal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CopiaFotos4K
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            listBox1.Items.Add("Iniciando a verificação...");
+
+            DirectoryInfo dirInfo = new DirectoryInfo(txtFolder.Text.Trim());
+            if (dirInfo.Exists)
+            {
+                listBox1.Items.Add("Criando diretório...");
+                Directory.CreateDirectory(dirInfo.FullName + "\\4K");
+
+                // PARA CADA ARQUIVO (CONF)...
+                // MOVER TODOS OS ARQUIVOS COM O MESMO NOME (NAO CONF)
+                // RENOMEAR O ARQUIVO ATUAL (RETIRANDO O CONF)
+                listBox1.Items.Add("Verificando arquivos (CONV)...");
+                var list = dirInfo.GetFiles("* (CONV).*");
+                foreach (var fi in list)
+                {
+                    listBox1.Items.Add(fi.Name);
+
+                    var sSimpleName = fi.Name.Replace(" (CONV)", "");
+                    var sOtherFiles = sSimpleName.Replace(fi.Extension, ".*");
+                    var listOther = dirInfo.GetFiles(sOtherFiles);
+                    foreach (var fo in listOther)
+                    {
+                        listBox1.Items.Add($"Movendo arquivo {fo.Name}...");
+                        File.Move(fo.FullName, $"{dirInfo.FullName}\\4K\\{fo.Name}");
+                    }
+
+                    listBox1.Items.Add("Renomeando arquivo original...");
+                    File.Move(fi.FullName, $"{dirInfo.FullName}\\{sSimpleName}");
+                }
+
+                // APAGAR ARQUIVOS "AAE"
+                listBox1.Items.Add("Verificando arquivos AAE...");
+                list = dirInfo.GetFiles("*.AAE");
+                foreach (var fi in list)
+                {
+                    listBox1.Items.Add($"Excluindo arquivo {fi.Name}...");
+                    File.Delete(fi.FullName);
+                }
+
+            } else 
+            {
+                listBox1.Items.Add("Diretório não encontrado.");
+            }
+        }
+
+    }
+}
