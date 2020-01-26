622b81355d392299ede69ce5500a61e07072cbc3
Rela <marciorela75@gmail.com>
2020-01-26 11:14:39 -0300

    Verificar arquivos não convertidos

--- CopiaFotos4K/frmPrincipal.cs
+++ CopiaFotos4K/frmPrincipal.cs
@@ -35,11 +35,30 @@ namespace CopiaFotos4K
                 listBox1.Items.Add("Criando diretório...");
                 Directory.CreateDirectory(dirInfo.FullName + "\\4K");
 
+                // PRIMEIRO, VERIFICA SE EXISTE ALGUM ARQUIVO
+                // QUE NÃO TENHA SUA CORRESPONDÊNCIA (CONV)
+                listBox1.Items.Add("");
+                listBox1.Items.Add("Verificando se todos os arquivos foram convertidos...");
+                var list = dirInfo.GetFiles("*.*");
+                foreach (var fi in list)
+                {
+                    if (!fi.Name.Contains("(CONV)"))
+                    {
+                        var sFile = fi.Name.Replace(fi.Extension, $" (CONV).*");
+                        var listOther = dirInfo.GetFiles(sFile);
+                        if (listOther.Count() == 0)
+                        {
+                            listBox1.Items.Add($"Arquivo não convertido: {fi.Name}");
+                        }
+                    }
+                }
+
                 // PARA CADA ARQUIVO (CONF)...
                 // MOVER TODOS OS ARQUIVOS COM O MESMO NOME (NAO CONF)
                 // RENOMEAR O ARQUIVO ATUAL (RETIRANDO O CONF)
+                listBox1.Items.Add("");
                 listBox1.Items.Add("Verificando arquivos (CONV)...");
-                var list = dirInfo.GetFiles("* (CONV).*");
+                list = dirInfo.GetFiles("* (CONV).*");
                 foreach (var fi in list)
                 {
                     listBox1.Items.Add(fi.Name);
@@ -58,6 +77,7 @@ namespace CopiaFotos4K
                 }
 
                 // APAGAR ARQUIVOS "AAE"
+                listBox1.Items.Add("");
                 listBox1.Items.Add("Verificando arquivos AAE...");
                 list = dirInfo.GetFiles("*.AAE");
                 foreach (var fi in list)
@@ -65,7 +85,6 @@ namespace CopiaFotos4K
                     listBox1.Items.Add($"Excluindo arquivo {fi.Name}...");
                     File.Delete(fi.FullName);
                 }
-
             } else 
             {
                 listBox1.Items.Add("Diretório não encontrado.");
